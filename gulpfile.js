4e5ebecdfe8bbe6ff03169b815c0d717795117d8
lazamar <lazaronijunior@gmail.com>
2016-11-11 16:15:46 +0000

    Fix reference to example file in browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ organiser.registerAll('./gulp-tasks', {
   'browser-sync': {
     src: '.', // it doesn't matter, it's just so the task object is not ignored.
     reloadOn: ['transpile-react'], // reload page when these tasks happen
-    startPath: 'examples/barebones.html',
+    startPath: 'examples/bookings.html',
     baseDir: './',
   },
   'karma-test': {
