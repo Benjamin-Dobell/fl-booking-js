3a1559fe08d8cf5f64a2a125cd6c04eec4f8e9f8
lazamar <lazaronijunior@gmail.com>
2016-11-14 09:24:36 +0000

    Event creation test

--- tests/unit-spec.js
+++ tests/unit-spec.js
@@ -1,6 +1,33 @@
 /* eslint-env jasmine */
 /* global flBooking, createFakeEvents */
 
+describe('smoke tests', () => {
+  it('flBooking should be defined', () => {
+    expect(flBooking).toBeDefined();
+  });
+
+  it('should throw if instantiated without a config object', () => {
+    expect(flBooking).toThrow();
+  });
+
+  it('shoud initiate propperly', () => {
+    const init = () => flBooking({
+      targetEl: document.createElement('div'),
+      autofillUser: 'James',
+      autofillEmail: 'james@brown.com',
+      timezone: {
+        timezone: 'Europe/London',
+        utc_offset: 0,
+      },
+      // must return a promise
+      createBooking: data => { console.log(data); return Promise.resolve(data); },
+      getEvents: () => createFakeEvents(1000),
+    });
+
+    expect(init).not.toThrow();
+  });
+});
+
 describe('fl-booking', () => {
   let wrapper;
   let container;
@@ -17,17 +44,9 @@ describe('fl-booking', () => {
     wrapper.appendChild(container);
   });
 
-  it('flBooking should be defined', () => {
-    expect(flBooking).toBeDefined();
-  });
-
-  it('should throw if instantiated without a config object', () => {
-    expect(flBooking).toThrow();
-  });
-
-  it('shoud initiate propperly', () => {
-    const init = () => flBooking({
-      targetEl: document.querySelector('.booking-target'),
+  it('creates events', done => {
+    flBooking({
+      targetEl: container,
       autofillUser: 'James',
       autofillEmail: 'james@brown.com',
       timezone: {
@@ -39,11 +58,12 @@ describe('fl-booking', () => {
       getEvents: () => createFakeEvents(1000),
     });
 
-    document.body.innerHTML = `
-    <h1 class="title">fl-booking</h1>
-    <div class="container">
-      <div class="booking-target"></div>
-    </div>`;
-    expect(init).not.toThrow();
+
+    setTimeout(() => {
+      expect(
+        container.querySelectorAll('.fc-event').length
+      ).toBeGreaterThan(0);
+      done();
+    }, 1000);
   });
 });
